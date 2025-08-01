162731e62bd853766f9d19426ace2cfe69a7bab8
thunderking-nx <kaushikdonthula@gmail.com>
2025-08-01 11:26:09 +0530

    Add files via upload

---
+++ Typecast.java
@@ -0,0 +1,25 @@
+package skill1;
+
+public class Typecast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a;
+byte b;
+b=120;
+a=b;
+System.out.println("a = " + a);
+System.out.println(" b " + b);
+char ch = 'G';
+boolean x;
+long p;
+byte y;
+p = 12500;
+y = (byte) p;
+System.out.println("p = "+ p);
+System.out.println("y= "+ y);
+
+
+	}
+
+}
